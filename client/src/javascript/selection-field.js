bcde2179e6bc2117697cec4a6892139697c2c349
Bettina Bröthaler <bettina.broethaler@gmx.at>
2023-09-05 09:14:21 +0200

    Added event lisener to replease feather icons on button aciton click

--- client/src/javascript/selection-field.js
+++ client/src/javascript/selection-field.js
@@ -1,3 +1,17 @@
 import feather from 'feather-icons';
+document.addEventListener('DOMContentLoaded', () => {
+    feather.replace();
+    initActionButtons();
+})
 
-feather.replace();
+function initActionButtons() {
+    let actionButtons = document.querySelectorAll('button.btn.action');
+    actionButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            setTimeout(() => {
+                feather.replace();
+                initActionButtons();
+            }, 1000);
+        })
+    })
+}
