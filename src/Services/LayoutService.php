5bf41b9d09f1f17fc02d699ab65b5439c4141760
Bettina Bröthaler <bettina.broethaler@gmx.at>
2023-12-07 13:05:18 +0100

    Maded Layoutservice Options configurabe via yml

--- src/Services/LayoutService.php
+++ src/Services/LayoutService.php
@@ -3,11 +3,13 @@
 namespace Pixelpoems\LayoutOptions\Services;
 
 use SilverStripe\Control\Controller;
+use SilverStripe\Core\Config\Configurable;
 use SilverStripe\Core\Injector\Injectable;
 
 class LayoutService extends Controller
 {
     use Injectable;
+    use Configurable;
 
     /**
      * Used within classes: Text
@@ -15,20 +17,24 @@ class LayoutService extends Controller
      */
     public function getHeadingTagOptions(): array
     {
-        $options = [
-            'h2' => [
-                'Value' => 'h2',
-                'Title' => 'H2',
-            ],
-            'h3' => [
-                'Value' => 'h3',
-                'Title' => 'H3',
-            ],
-            'h4' => [
-                'Value' => 'h4',
-                'Title' => 'H4',
-            ],
-        ];
+        $options = self::config()->get('heading_tag_options');
+
+        if(!$options) {
+            $options = [
+                'h2' => [
+                    'Value' => 'h2',
+                    'Title' => 'H2',
+                ],
+                'h3' => [
+                    'Value' => 'h3',
+                    'Title' => 'H3',
+                ],
+                'h4' => [
+                    'Value' => 'h4',
+                    'Title' => 'H4',
+                ],
+            ];
+        }
 
         $this->extend('updateHeadingTagOptions', $options);
         return $options;
@@ -40,10 +46,14 @@ class LayoutService extends Controller
      */
     public function getTextColorOptions(): array
     {
-        $options = [
-            'black' => '#000',
-            'white' => '#fff'
-        ];
+        $options = self::config()->get('text_color_options');
+
+        if(!$options) {
+            $options = [
+                'black' => '#000',
+                'white' => '#fff'
+            ];
+        }
 
         $this->extend('updateTextColorOptions', $options);
         return $options;
@@ -55,26 +65,30 @@ class LayoutService extends Controller
      */
     public function getAlignOptions(): array
     {
-        $options = [
-            'left' => [
-                'Value' => 'l',
-                'Title' => _t('LayoutOptions.Left', "Left"),
-                'ShowTitle' => true,
-                'Icon' => 'align-left'
-            ],
-            'center' => [
-                'Value' => 'c',
-                'Title' => _t('LayoutOptions.Center', "Center"),
-                'ShowTitle' => true,
-                'Icon' => 'align-center'
-            ],
-            'right' => [
-                'Value' => 'r',
-                'Title' => _t('LayoutOptions.Right', "Right"),
-                'ShowTitle' => true,
-                'Icon' => 'align-right'
-            ],
-        ];
+        $options = self::config()->get('align_options');
+
+        if(!$options) {
+            $options = [
+                'left' => [
+                    'Value' => 'l',
+                    'Title' => _t('LayoutOptions.Left', "Left"),
+                    'ShowTitle' => true,
+                    'Icon' => 'align-left'
+                ],
+                'center' => [
+                    'Value' => 'c',
+                    'Title' => _t('LayoutOptions.Center', "Center"),
+                    'ShowTitle' => true,
+                    'Icon' => 'align-center'
+                ],
+                'right' => [
+                    'Value' => 'r',
+                    'Title' => _t('LayoutOptions.Right', "Right"),
+                    'ShowTitle' => true,
+                    'Icon' => 'align-right'
+                ],
+            ];
+        }
 
         $this->extend('updateAlignOptions', $options);
         return $options;
@@ -86,26 +100,30 @@ class LayoutService extends Controller
      */
     public function getLayoutWidthOptions(): array
     {
-        $options = [
-            'small' => [
-                'Value' => 'sm',
-                'Title' => _t('LayoutOptions.Small', "Small"),
-                'ShowTitle' => true,
-                'Content' => 'SM'
-            ],
-            'medium' => [
-                'Value' => 'md',
-                'Title' => _t('LayoutOptions.Medium', "Medium"),
-                'ShowTitle' => true,
-                'Content' => 'M'
-            ],
-            'large' => [
-                'Value' => 'lg',
-                'Title' => _t('LayoutOptions.Large', "Large"),
-                'ShowTitle' => true,
-                'Content' => 'LG'
-            ],
-        ];
+        $options = self::config()->get('layout_width_options');
+
+        if(!$options) {
+            $options = [
+                'small' => [
+                    'Value' => 'sm',
+                    'Title' => _t('LayoutOptions.Small', "Small"),
+                    'ShowTitle' => true,
+                    'Content' => 'SM'
+                ],
+                'medium' => [
+                    'Value' => 'md',
+                    'Title' => _t('LayoutOptions.Medium', "Medium"),
+                    'ShowTitle' => true,
+                    'Content' => 'M'
+                ],
+                'large' => [
+                    'Value' => 'lg',
+                    'Title' => _t('LayoutOptions.Large', "Large"),
+                    'ShowTitle' => true,
+                    'Content' => 'LG'
+                ],
+            ];
+        }
 
         $this->extend('updateLayoutWidthOptions', $options);
         return $options;
@@ -118,12 +136,17 @@ class LayoutService extends Controller
      */
     public function getBackgroundColorOptions(): array
     {
-        $options = [
-            'white' => '#fff',
-            'light' => '#ddd',
-            'dark' => '#aaa',
-            'black' => '#000'
-        ];
+        $options = self::config()->get('background_color_options');
+
+        if(!$options) {
+            $options = [
+                'white' => '#fff',
+                'light' => '#ddd',
+                'dark' => '#aaa',
+                'black' => '#000'
+            ];
+        }
+
 
         $this->extend('updateBackgroundColorOptions', $options);
         return $options;
